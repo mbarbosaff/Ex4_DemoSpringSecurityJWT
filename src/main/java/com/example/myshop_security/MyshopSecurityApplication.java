03d60d6c26078592a3d82b59a67cc8f501f7d304
miguelfigueiredo <mbarbosaff@gmail.com>
2026-03-08 23:00:14 +0000

    Initial commit

---
+++ src/main/java/com/example/myshop_security/MyshopSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.myshop_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyshopSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyshopSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myshop_security/MyshopSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myshop_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyshopSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
